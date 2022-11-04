35198086c31c120725cf3bc20fbe40a1ba550c3e
509 <509@academicos.cesde.edu.co>
2022-11-04 17:37:25 -0500

    Proyecto django nov 4

--- configuracion/web/views.py
+++ configuracion/web/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 
+from web.formularios.formularioMedico import formularioMedico
+
 # Create your views here.
 #render (renderizar) es PINTAR 
 def Home(request):
     return render(request,'index.html')
 
 def Medicos(request):
-    return render(request,'registromedicos.html')
+
+    #debo utilizar la clase formularioMedico
+    # Creamos asi un objeto 
+    formulario = formularioMedico()
+    diccionario = {
+        "formulario" : formulario
+    }
+
+    #Activar la recepcion de datos
+    if request.method=='POST':
+        #validar si los datos son correctos
+        datosRecibidos=formularioMedico(request.POST)
+        if datosRecibidos.is_valid():
+            #capturar los datos
+            datos=datosRecibidos.cleaned_data
+            print(datos)
+
+    return render(request,'registromedicos.html',diccionario)
